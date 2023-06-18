f540493610e928c66ba2f341cf1c51285c17d4b5
Srinath Venkatesh <srinathvb1@gmail.com>
2023-06-17 22:17:51 -0400

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main className="flex flex-col justify-center items-center h-5/6">
             <p>Games</p>
+            <p>Testing</p>
         </main>
     );
 }
